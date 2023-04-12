cf6c2d70f52eb8eea5bd289881e5abdc7891caa4
Tiago Leite <contato@tiagocreator.com>
2023-04-12 03:02:47 -0300

    Add default components exports

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as ActionModal } from './actionModal/ActionModal';
+export { default as GlobalState } from './globalState/GlobalState';
+export { default as Header } from './header/Header';
+export { default as PokemonCard } from './pokemonCard/PokemonCard';
+export { default as ProgressBar } from './progressBar/ProgressBar';
