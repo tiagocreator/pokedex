ccfc42b9812b0dd262ef42f5af51324f7f358609
Tiago Leite <contato@tiagocreator.com>
2023-04-12 02:56:40 -0300

    Update vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react()],
-})
+  esbuild: {
+    loader: 'jsx',
+    include: ['src/**/*.jsx', 'node_modules/**/*.jsx', 'src/**/*.js', 'node_modules/**/*.js'],
+  },
+}));
