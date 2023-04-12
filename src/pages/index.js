865d7f10c22f414adaa12ce1529319297243cf70
Tiago Leite <contato@tiagocreator.com>
2023-04-12 03:03:03 -0300

    Add default pages exports

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Details } from './details/Details';
+export { default as Error404 } from './error404/Error404';
+export { default as Home } from './home/Home';
+export { default as Pokedex } from './pokedex/Pokedex';
