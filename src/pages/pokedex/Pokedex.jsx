6e0fccada9efdad8d791e493d4a5b1655f94c3a0
Tiago Leite <contato@tiagocreator.com>
2023-04-12 03:09:16 -0300

    Create pokedex page

---
+++ src/pages/pokedex/Pokedex.jsx
@@ -0,0 +1,40 @@
+import { useContext } from 'react';
+
+import GlobalContext from '../../context/globalContext';
+
+import { ActionModal, Header, PokemonCard } from '../../components/index';
+
+import { MainGeral, ContainerTitle, Title } from '../../GlobalStyles';
+import { ContainerPokedex } from '../styles/PokedexStyles';
+
+const Pokedex = () => {
+  const { pokedexList, setPokedexList, setModal, setSelect } = useContext(GlobalContext);
+
+  return (
+    <ContainerPokedex>
+      <ActionModal />
+      <Header page={'pokedex'} />
+      <ContainerTitle>
+        <Title>Meus Pokémons</Title>
+      </ContainerTitle>
+      {pokedexList && (
+        <MainGeral>
+          {pokedexList?.map((item) => {
+            return (
+              <PokemonCard
+                key={item}
+                setModal={setModal}
+                setSelect={setSelect}
+                poke={{ name: item }}
+                pokedexList={pokedexList}
+                setPokedexList={setPokedexList}
+              />
+            );
+          })}
+        </MainGeral>
+      )}
+    </ContainerPokedex>
+  );
+};
+
+export default Pokedex;
