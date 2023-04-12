6802b085ef088fb7336adae2e6c92d19640cc57b
Tiago Leite <contato@tiagocreator.com>
2023-04-12 02:59:11 -0300

    Add data files, images, types, colors and stats

---
+++ src/data.js
@@ -0,0 +1,64 @@
+import bug from './assets/img/bug.png';
+import dark from './assets/img/dark.png';
+import dragon from './assets/img/dragon.png';
+import electric from './assets/img/electric.png';
+import fairy from './assets/img/fairy.png';
+import fighting from './assets/img/fighting.png';
+import fire from './assets/img/fire.png';
+import ghost from './assets/img/ghost.png';
+import grass from './assets/img/grass.png';
+import ground from './assets/img/ground.png';
+import ice from './assets/img/ice.png';
+import normal from './assets/img/normal.png';
+import poison from './assets/img/poison.png';
+import psychic from './assets/img/psychic.png';
+import rock from './assets/img/rock.png';
+import steel from './assets/img/steel.png';
+import water from './assets/img/water.png';
+import flying from './assets/img/flying.png';
+
+export const types = {
+  bug: bug,
+  dark: dark,
+  dragon: dragon,
+  electric: electric,
+  fairy: fairy,
+  fighting: fighting,
+  flying: flying,
+  fire: fire,
+  ghost: ghost,
+  ground: ground,
+  grass: grass,
+  ice: ice,
+  normal: normal,
+  poison: poison,
+  psychic: psychic,
+  rock: rock,
+  steel: steel,
+  water: water,
+};
+
+export const colors = {
+  bug: '#76A866',
+  dark: '#705848',
+  dragon: '#004170',
+  electric: '#FAED61',
+  fairy: '#EE99AC',
+  fighting: '#C02038',
+  flying: '#a890f0',
+  fire: '#EAAB7D',
+  ghost: '#705898',
+  ground: '#e0c068',
+  grass: '#729F92',
+  ice: '#98d8d8',
+  normal: '#BF9762',
+  poison: '#9966CC',
+  psychic: '#F85888',
+  rock: '#b8a038',
+  steel: '#b8b8d0',
+  water: '#71C3FF',
+};
+
+export const stats = ['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed'];
+
+export const baseUrl = 'https://pokeapi.co/api/v2/';
