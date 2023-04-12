ff8f480b582a477f49fc727a478f3240d505c229
Tiago Leite <contato@tiagocreator.com>
2023-04-12 03:01:12 -0300

    Add main file content and global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { createGlobalStyle } from 'styled-components';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#root, body{
+max-width: 100vw;
+min-height: 100vh;
+}
+
+#root{
+  display: flex;
+  flex-direction: column;
+}
+
+button {
+  cursor: pointer;
+}
+`;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
-)
+);
