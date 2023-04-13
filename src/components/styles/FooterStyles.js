cae265873f3d9d981688b9e58ee11cf386ea76e6
Tiago Leite <contato@tiagocreator.com>
2023-04-12 21:32:40 -0300

    Create footer component styles

---
+++ src/components/styles/FooterStyles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const FooterStyle = styled.footer`
+  margin-block-start: auto;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+  font-weight: 700;
+  font-size: 1.3rem;
+
+  a {
+    margin-inline-start: 5px;
+    text-decoration: none;
+    color: #3e5ab1;
+  }
+`;
