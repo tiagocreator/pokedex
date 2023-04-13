80dae97c3e34880974d97bdbf1b736e7a3c1ad54
Tiago Leite <contato@tiagocreator.com>
2023-04-12 21:33:14 -0300

    Update soft error 404 page styles

--- src/pages/styles/Error404Styles.js
+++ src/pages/styles/Error404Styles.js
@@ -1,6 +1,11 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Error404Style = styled.div`
   display: flex;
   justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 1.5rem;
+  font-weight: 700;
+  height: 663px;
 `;
