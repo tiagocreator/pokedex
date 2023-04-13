52cf9acf88edfeb697e0dd0574388bac7d4211a1
Tiago Leite <contato@tiagocreator.com>
2023-04-12 21:32:16 -0300

    Update default components exports

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as GlobalState } from './globalState/GlobalState';
 export { default as Header } from './header/Header';
 export { default as PokemonCard } from './pokemonCard/PokemonCard';
 export { default as ProgressBar } from './progressBar/ProgressBar';
+export { default as Footer } from './footer/Footer';
